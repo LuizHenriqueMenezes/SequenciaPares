bb812391983d3524fc61f7c0430c05b93ae881e8
Luiz Henrique <lmenezesmessias@gmail.com>
2021-05-31 09:48:11 -0300

    Finalização do programa

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SequenciaPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine(" \n----- SEQUÊNCIA PARES ----- \n");
+            Console.ResetColor();
+
+            Console.Write("Insira um número inteiro: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            int i = 0;
+
+            while (i <= x)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+            
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("\nObrigado por usar nosso programa. Pressione qualquer tecla para finalizar. ");
+            Console.ReadKey();
+            Console.ResetColor();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# SequenciaPares
-Receba um número inteiro positivo. Exiba todos os números pares entre zero e o número digitado (inclusive).  Ex.:  Números pares entre 0 e ? 15 0 2 4 6 8 10 12 14
+# Sequencia Pares
+
+Receba um número inteiro positivo. Exiba todos os números pares entre zero e o número digitado (inclusive).  
+
+Ex.:  
+
+```cs
+Números pares entre 0 e ? 15 0 2 4 6 8 10 12 14
+```
+
+Feito na [ETEC Adolpho Berezin](http://eteab.com.br/cms/)
